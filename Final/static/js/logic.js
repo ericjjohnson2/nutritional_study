2707c19e801a0c58bae3fce06585d2867d6a0e5d
lvit001 <140283164+lvit001@users.noreply.github.com>
2024-01-31 16:51:21 -0600

    made some stylistic updates to the map legend and popups

--- Final/static/js/logic.js
+++ Final/static/js/logic.js
@@ -139,9 +139,7 @@ d3.json(url).then(function(data) {
         newMarker.addTo(layers[restaurantName]);
 
         // Bind a popup to the marker that will  display on being clicked. This will be rendered as HTML.
-        newMarker.bindPopup(`<h1>${data[i].Restaurant}</h1><hr><p> 
-        Street Address: ${data[i].Address}</p>
-        <hr><p> City: ${data[i].City}</p>`);
+        newMarker.bindPopup(`<h2>${data[i].Restaurant}</h2><hr><p>${data[i].Address}, ${data[i].City}</p>`);
 
         // center the map on a marker when you double click it
         // code found here for fit bounds: https://jeffreymorgan.io/articles/how-to-center-a-leaflet-map-on-a-marker/
@@ -160,8 +158,8 @@ var legend = L.control({position: 'bottomleft'});
 
 legend.onAdd = function(map) {
     var div = L.DomUtil.create("div", "legend");
-    div.innerHTML += "<h3>Restaurants</h3>";
-    div.innerHTML += "<h5>*Double click marker to zoom in*</h5>";
+    div.innerHTML += "<h3>Fast Food Restaurants in the Twin Cities</h3>";
+    div.innerHTML += "<h6>*Double click a marker to zoom in*</h6>";
     div.innerHTML += '<i style="background: #A42409"></i><span>McDonalds</span><br>';
     div.innerHTML += '<i style="background: #FFC300"></i><span>Burger King</span><br>';
     div.innerHTML += '<i style="background: #6A2C89"></i><span>Taco Bell</span><br>';
