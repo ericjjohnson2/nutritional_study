1e3eb804f7cc64803db5f3ab3bccd8c468b0ec7f
ericjjohnson2 <144396358+ericjjohnson2@users.noreply.github.com>
2024-02-03 19:16:35 -0600

    organized code in proposed final folder

---
+++ Proposed Final/README.md
@@ -0,0 +1,58 @@
+# Project-3-Fastfood Topic: Nutritional Content of Fast Food
+
+## An overview of the project and its purpose
+Group 3 focused on data visualization of the nutritional content of food sold at eight major chain fast food restaurants located in the [Twin Cities, Minnesota](https://en.wikipedia.org/wiki/Minneapolis%E2%80%93Saint_Paul). Our datasets will allow users to analyze/compare several nutritional values:
+*	calories
+*	protein
+*	sodium
+*	carbohydrates
+*	fat
+*	sugar
+*	cholesterol
+*	fiber
+
+We have created visualizations to display the nutritional content of specific foods and allow users to compare food items. We also created an interactive quiz to help visitors make informed choices when ordering some of America's favorite fast foods.
+  
+## Instructions on how to use and interact with the project
+![image](https://github.com/Chud-rf/Project-3-Fastfood/assets/140283164/305333ac-ed79-4aa6-b055-4431e04e66cc)
+- Upon loading the index.html file, users will be directed to a homepage featuring a Leaflet map. This map plots the locations of 8 fast-food restaurants in the Twin Cities metro area. The map contains a legend that corresponds to the color-coded markers, which differentiate the various restaurants. Users can toggle certain restaurants on and off using the overlay in the top right corner. When users click on a marker, a popup displays the restaurant’s name, address, and user rating provided by Google Maps. A double-click on a marker also triggers a zoom function, enabling users to examine the selected restaurant and its surrounding area more closely.
+
+![image](https://github.com/Chud-rf/Project-3-Fastfood/blob/main/Final/static/img/viz%20screenshot.png)
+- Users can interact with [PyGWalker](https://docs.kanaries.net/pygwalker), a newer Python Library for Exploratory Data Analysis with Visualization. The PyGWalker visualizations show the median or mean macro- and micronutrients across all fast food restaurants, as well as the relationships between select nutrients. Last, nutrient relationships are compared between the two burger chains.
+- Users can navigate to the fast food quiz to learn about nutritional content and learn fun facts about fast food.
+
+## Ethical considerations made in the project:
+Ethical considerations made for this project: we are assuming fast food is unhealthy. We chose to not utilize web scraping for pricing data since fast food companies as well as websites with pricing data specifically stated we could not scrape data. The source of the dataset is Kaggle.com and the Fastfood Nutrition dataset references this license: and the Top 50 Fast-Food Chains in the USA dataset license is created from QSR Magazine.
+
+After researching the restaurants’ Terms of Service posted on their websites, we decided not to post any company logos on our project. Doing so would require written consent from many of the chains since a company’s logo and marks are viewed as proprietary material. Burger King’s website was the strictest, listing out several types of prohibited content and activities, not limited to spidering, scraping, harvesting, reverse engineering, deciphering, or attempting to circumvent any software that’s part of the Services. All chains' websites referenced the Children’s Online Privacy Protection Act of 1998.
+
+## References for the data source(s):
+* PyGWalker references:
+
+* https://github.com/Kanaries/pygwalker
+* https://www.youtube.com/watch?v=Ynt7Etci1KU
+* https://stackoverflow.com/questions/36351109/ipython-notebook-ipywidgets-does-not-show
+
+* Flask reference:
+*  https://www.digitalocean.com/community/tutorials/how-to-use-an-sqlite-database-in-a-flask-application
+
+* Dataset:
+*  https://www.kaggle.com/datasets/ulrikthygepedersen/fastfood-nutrition [Which fast food products are worst for you?]
+
+*  Quiz Facts:
+*  https://www.fda.gov/food/food-labeling-nutrition/nutrition-education-resources-materials
+*  https://www.cspinet.org/protecting-our-health/menu-labeling
+*  https://www.mayoclinic.org/healthy-lifestyle/nutrition-and-healthy-eating/in-depth/carbohydrates/art-20045705#:~:text=How%20many%20carbohydrates%20do%20you,grams%20of%20carbs%20a%20day.
+
+## References for any code used that is not your own
+- Basic Quiz Code
+     - https://medium.com/@codepicker57/building-an-interactive-quiz-with-html-css-and-javascript-efe9bd8129e2
+- Flask app route code - assistance from ChatGPT
+- Map Code
+     - Help to set the background color of the legend [here](https://codepen.io/haakseth/pen/KQbjdO). 
+     - Code found here for fit bounds in leaflet found [here](https://jeffreymorgan.io/articles/how-to-center-a-leaflet-map-on-a-marker/).
+     - Support setting up the legend found [here](https://codepen.io/haakseth/pen/KQbjdO) and html color identification found [here](https://htmlcolorcodes.com/).
+     - Code for word-wrap in legend found [here](https://stackoverflow.com/questions/3587390/how-can-i-make-text-appear-on-next-line-instead-of-overflowing)
+
+
+Chad's code snippet: ![Alt text](chad-scratch-work/API_Pull_code.png)
---
+++ Proposed Final/app/static/js/logic.js
@@ -0,0 +1,204 @@
+// assign the local url to a constant variable
+const url = "http://127.0.0.1:5000/api/location"
+// const summaryURL = "http://127.0.0.1:5000/api/summary";
+
+// Create the tile layer that will be the background of our map.
+let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// Initialize all the LayerGroups that we'll use.
+let layers = {
+    McDonalds: new L.LayerGroup(),
+    Burger_King: new L.LayerGroup(),
+    Taco_Bell: new L.LayerGroup(),
+    Chick_Fil_A: new L.LayerGroup(),
+    Sonic: new L.LayerGroup(),
+    Arbys: new L.LayerGroup(),
+    Dairy_Queen: new L.LayerGroup(),
+    Subway: new L.LayerGroup(),
+};
+
+// Create the map with our layers.
+let map = L.map("map-id", {
+    center: [44.94, -93.26],
+    zoom: 10,
+    layers: [
+      layers.McDonalds,
+      layers.Burger_King,
+      layers.Taco_Bell,
+      layers.Chick_Fil_A,
+      layers.Sonic,
+      layers.Arbys,
+      layers.Dairy_Queen,
+      layers.Subway,
+    ]
+});
+
+// Add our "streetmap" tile layer to the map.
+streetmap.addTo(map);
+
+// Create an overlays object to add to the layer control.
+let overlays = {
+    "McDonald's": layers.McDonalds,
+    "Burger King": layers.Burger_King,
+    "Taco Bell": layers.Taco_Bell,
+    "Chick-fil-A": layers.Chick_Fil_A,
+    "Sonic Drive In": layers.Sonic,
+    "Arby's": layers.Arbys,
+    "Dairy Queen": layers.Dairy_Queen,
+    "Subway": layers.Subway,
+};
+
+console.log(`Map: ${map}`);
+
+// Create a control for our layers, and add our overlays to it.
+L.control.layers(null, overlays, {
+    collapsed: false
+}).addTo(map);
+
+
+// Initialize an object that contains icons for each layer group.
+let icons = {
+    McDonalds: L.ExtraMarkers.icon({
+      markerColor: "red",
+      shape: "circle"
+    }),
+
+    Burger_King: L.ExtraMarkers.icon({
+      markerColor: "yellow",
+      shape: "circle"
+    }),
+
+    Taco_Bell: L.ExtraMarkers.icon({
+      markerColor: "purple",
+      shape: "circle",
+    }),
+    Chick_Fil_A: L.ExtraMarkers.icon({
+        markerColor: "orange",
+        shape: "circle"
+    }),
+
+    Sonic: L.ExtraMarkers.icon({
+        markerColor: "blue",
+        shape: "circle"
+    }),
+
+    Arbys: L.ExtraMarkers.icon({
+        markerColor: "black",
+        shape: "circle",
+    }),
+    Dairy_Queen: L.ExtraMarkers.icon({
+        markerColor: "pink",
+        shape: "circle"
+    }),
+
+    Subway: L.ExtraMarkers.icon({
+        markerColor: "green",
+        shape: "circle"
+    }),
+};
+
+// Perform an API call to the places dataset
+d3.json(url).then(function(data) {
+    console.log(data)
+    // Initialize restaurantName, which will be used as a key to access the appropriate layers and icons
+    let restaurantName;
+
+    // use a for loop to cycle through the locations
+    for (let i = 0; i < data.length; i++) {
+        
+        // if (data[i].Restaurant == "McDonald's") {
+        //     restaurantName = "McDonalds";
+        // }
+        // else if (data[i].Restaurant == "Burger King") {
+        //     restaurantName = "Burger_King";
+        // }
+        // else if (data[i].Restaurant == "Taco Bell") {
+        //     restaurantName = "Taco_Bell";
+        // }
+        // else if (data[i].Restaurant == "Chick-fil-A") {
+        //     restaurantName = "Chick_Fil_A";
+        // }
+        // else if (data[i].Restaurant == "Sonic Drive-In") {
+        //     restaurantName = "Sonic";
+        // }
+        // else if (data[i].Restaurant == "Arby's") {
+        //     restaurantName = "Arbys";
+        // }
+        // else if (data[i].Restaurant == "Dairy Queen") {
+        //     restaurantName = "Dairy_Queen";
+        // }
+        // else if (data[i].Restaurant == "Subway") {
+        //     restaurantName = "Subway";
+        // }
+
+        if (data[i].Restaurant.includes("McDonald's")) {
+            restaurantName = "McDonalds";
+        }
+        else if (data[i].Restaurant.includes("Burger King")) {
+            restaurantName = "Burger_King";
+        }
+        else if (data[i].Restaurant.includes("Taco Bell")) {
+            restaurantName = "Taco_Bell";
+        }
+        else if (data[i].Restaurant.includes("Chick-fil-A")) {
+            restaurantName = "Chick_Fil_A";
+        }
+        else if (data[i].Restaurant.includes("Sonic Drive-In")) {
+            restaurantName = "Sonic";
+        }
+        else if (data[i].Restaurant.includes("Arby's")) {
+            restaurantName = "Arbys";
+        }
+        else if (data[i].Restaurant.includes("Dairy Queen")) {
+            restaurantName = "Dairy_Queen";
+        }
+        else if (data[i].Restaurant.includes("Subway")) {
+            restaurantName = "Subway";
+        }
+
+        // Create a new marker with the appropriate icon and coordinates.
+        let newMarker = L.marker([data[i].Lat, data[i].Long_], {
+            icon: icons[restaurantName]
+            });
+
+        // Add the new marker to the appropriate layer.
+        newMarker.addTo(layers[restaurantName]);
+
+        // Bind a popup to the marker that will  display on being clicked. This will be rendered as HTML.
+        newMarker.bindPopup(`<h2>${data[i].Restaurant}</h2><hr><p>${data[i].Address}, ${data[i].City}</p>
+                            <hr><p>User Rating: ${data[i].Rating}</p>`);
+
+        // center the map on a marker when you double click it
+        // code found here for fit bounds: https://jeffreymorgan.io/articles/how-to-center-a-leaflet-map-on-a-marker/
+        newMarker.on('dblclick', function() {
+            var latLngs = [ newMarker.getLatLng() ];
+            var markerBounds = L.latLngBounds(latLngs);
+            map.fitBounds(markerBounds);  
+        });
+    }
+})
+
+// create a legend for the map
+// helpful code found here: https://codepen.io/haakseth/pen/KQbjdO
+// helpful website for finding numbers of colors found here: https://htmlcolorcodes.com/
+var legend = L.control({position: 'topright'});
+
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create("div", "legend");
+    div.innerHTML += "<h3>Fast Food Restaurants in the Twin Cities</h3>";
+    // div.innerHTML += "<h6>*Double click a marker to zoom in*</h6>";
+    div.innerHTML += '<i style="background: #A42409"></i><span>McDonalds</span><br>';
+    div.innerHTML += '<i style="background: #FFC300"></i><span>Burger King</span><br>';
+    div.innerHTML += '<i style="background: #6A2C89"></i><span>Taco Bell</span><br>';
+    div.innerHTML += '<i style="background: #FFA500"></i><span>Chick-fil-A</span><br>';
+    div.innerHTML += '<i style="background: #0B75D3"></i><span>Sonic Drive-In</span><br>';
+    div.innerHTML += '<i style="background: #000000"></i><span>Arbys</span><br>';
+    div.innerHTML += '<i style="background: #DA70D6"></i><span>Dairy Queen</span><br>';
+    div.innerHTML += '<i style="background: #008000"></i><span>Subway</span><br>';
+
+    return div;
+};
+
+legend.addTo(map);
